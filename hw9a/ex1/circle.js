c729b64fbcdab316ffbd960e3accd1aff9abf9bc
maexishere <alexandra.ablyazova@student.csulb.edu>
2022-04-21 12:23:36 -0700

    2

---
+++ hw9a/ex1/circle.js
@@ -0,0 +1,9 @@
+//let radius = Number(prompt("Your radius is: "))
+
+let radius = 3
+
+const p = Math.PI;
+
+module.exports.circumference = () => 2 * p * radius;
+
+module.exports.area = () => p * radius ** 2;
---
+++ hw9a/ex1/index.js
@@ -0,0 +1,4 @@
+const circle = require("./circle.js");
+
+//console.log(`The circumference and the area of a circle of radius ${radius} are ${circle.circumference(radius)} and ${circle.area(radius)}`);
+console.log(`The circumference and the area of a circle of radius 3 are ${circle.circumference(3)} and ${circle.area(3)}`);
